a6d0d2b04d3aeea23ad2cf4e27936e61b164fb4c
MardamBey <iam.mardambey@gmail.com>
2012-06-01 17:18:14 +0200

    Suppression d'un dossier DATA inutilisé dans la conf

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -7,12 +7,12 @@
 function get_file_icon($filename,$short_return=FALSE)
 {
   $extension = pathinfo($filename, PATHINFO_EXTENSION);
-  
+
   if($extension == "avi" || $extension == "mpeg" || $extension == "mp4" || $extension == "mkv") $extension = "avi";
   else if($extension == "mp3" || $extension == "midi" || $extension == "m4a" || $extension == "ogg" || $extension == "flac") $extension = "mp3";
   else if($extension == "iso" || $extension == "rar" || $extension == "zip") $extension = "iso";
   else $extension = "other";
-  
+
   if($short_return) return $extension;
   else return "ressources/ext/".$extension.".png";
 }
@@ -21,17 +21,17 @@ function get_file_icon($filename,$short_return=FALSE)
  * Convert size with readable units
  * @param $fs The size
  */
-function convert_size($fs) 
-{ 
-     if ($fs >= 1073741824) 
-      $fs = round($fs / 1073741824 * 100) / 100 . " Go"; 
-     elseif ($fs >= 1048576) 
-      $fs = round($fs / 1048576 * 100) / 100 . " Mo"; 
-     elseif ($fs >= 1024) 
-      $fs = round($fs / 1024 * 100) / 100 . " Ko"; 
-     else 
-      $fs = $fs . " o"; 
-     return $fs; 
+function convert_size($fs)
+{
+     if ($fs >= 1073741824)
+      $fs = round($fs / 1073741824 * 100) / 100 . " Go";
+     elseif ($fs >= 1048576)
+      $fs = round($fs / 1048576 * 100) / 100 . " Mo";
+     elseif ($fs >= 1024)
+      $fs = round($fs / 1024 * 100) / 100 . " Ko";
+     else
+      $fs = $fs . " o";
+     return $fs;
 }
 
 /**
@@ -41,18 +41,18 @@ function convert_size($fs)
 function recursive_directory_tree($directory = null)
 {
     global $listof_dir;
-    
+
     //If $directory is null, set $directory to the current working directory.
     if ($directory == null) {
         $directory = getcwd();
     }
-    
+
     //declare the array to return
     $return = array();
 
     //Check if the argument specified is an array
     if (is_dir($directory)) {
-    
+
         array_push($listof_dir,$directory);
         //Scan the directory and loop through the results
         foreach(scandir($directory) as $file) {
@@ -97,7 +97,7 @@ function print_tree_structure($treestructure,$filter="all",$editmode=FALSE)
     {
       $key = addslashes(basename($key));
       echo '<div class="onedir">';
-      if($editmode) echo '<input name="Files[]" id="Files" type="checkbox" value="'.htmlspecialchars($key).'" onclick="CheckLikes(this);"/>';        
+      if($editmode) echo '<input name="Files[]" id="Files" type="checkbox" value="'.htmlspecialchars($key).'" onclick="CheckLikes(this);"/>';
       echo '<img src="ressources/folder.png" onclick="showhidedir(\''.$key.'\');return false;" class="pointerLink imgfolder" />
       <span class="pointerLink" onclick="showhidedir(\''.$key.'\');return false;">'.$key.'</span></div>';
       echo '<div id="'.$key.'" class="dirInList" style="display:none;">';
@@ -113,7 +113,7 @@ function print_tree_structure($treestructure,$filter="all",$editmode=FALSE)
           ($filter == "seen" && file_exists("data/".basename($file)) && get_file_icon(basename($file),TRUE) == "avi"))
       {
         echo '<div style="margin-bottom:5px;" class="onefile" id="div-'.htmlspecialchars($file).'">';
-        if($editmode) echo '<input name="Files[]" id="Files" type="checkbox" value="'.htmlspecialchars($file).'"/>';         
+        if($editmode) echo '<input name="Files[]" id="Files" type="checkbox" value="'.htmlspecialchars($file).'"/>';
         echo '<a href="watch.php?file='.htmlspecialchars($file).'">';
         echo '<img src="'.get_file_icon($file).'" title="Stream or download this file" /></a> '.basename(htmlspecialchars($file)).'
           <a href="#" class="update_info">
@@ -172,26 +172,22 @@ function ustr_replace($needle , $replace , $haystack)
  */
 function check_dir()
 {
- 
-  $isdir_data = is_dir("data");
+
   $isdir_downloads = is_dir("downloads");
-  if(!$isdir_data || !$isdir_downloads)
+  if(!$isdir_downloads)
   {
     echo '<p style="background:#FF6B7A;padding:10px;color:#FFFFFF;margin-bottom:20px;">';
     echo '<span style="font-weight:bold;">WARNING</span><br/>';
-    if(!$isdir_data) echo "You must create a folder named \"data\" in the folder of Cakebox<br/>";
     if(!$isdir_downloads) echo "You must create a folder named \"downloads\" in the folder of Cakebox<br/>";
     echo '</p>';
   }
   else
   {
-    $chmod_data = substr(sprintf('%o', fileperms('data')),-3);
     $chmod_downloads = substr(sprintf('%o',fileperms('downloads')),-3);
     if($chmod_data != 777 || $chmod_downloads != 777)
     {
       echo '<p style="background:#FF6B7A;padding:10px;color:#FFFFFF;margin-bottom:20px;">';
       echo '<span style="font-weight:bold;">WARNING</span><br/>';
-      if($chmod_data != 777) echo "You must change the permissions of \"/data\" to 777 (chmod).<br/>";
       if($chmod_downloads != 777) echo "You must change the permissions of \"/downloads\" to 777 (chmod).<br/>";
       echo '</p>';
     }
