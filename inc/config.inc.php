d9ce8f02b1e29fb2d99e3eacd13b553aa0687a49
MardamBeyK <iam.mardambey@gmail.com>
2012-07-29 19:27:48 +0200

    Suppression de fichiers

--- inc/config.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('LOCAL_DL_PATH','downloads'); 		// The folder will be stored your file on your remote webserver
-                                                                                // /!\ Set it CHMOD 777
-?>
