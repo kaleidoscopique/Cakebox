00f5c77ff360f4e469cdc85aca59bd14aa164d96
MardamBey <iam.mardambey@gmail.com>
2013-02-10 00:49:40 +0100

    Petits changements sans importance

--- inc/header.php
+++ inc/header.php
@@ -5,12 +5,14 @@
 <link rel="stylesheet" href="ressources/reset.css" type="text/css" media="screen">
 <link href='http://fonts.googleapis.com/css?family=Changa+One|Droid+Sans:400,700' rel='stylesheet' type='text/css'>
 <link rel="icon" type="image/ico" href="favicon.ico" />
-<script type="text/javascript" src="ressources/jquery.leanModal.min.js"></script>
+<script type="text/javascript" src="ressources/jquery.leanModal.js"></script>
+<script type="text/javascript" src="ressources/jquery.sizzle.js"></script>
 
 <script>
-$(function() {
+$(document).ready( function(){ 
 	// Chargement du background configuré
 	$('body').css('background-image', 'url(ressources/backgrounds/<?php echo $config->get('background'); ?>)');
+	// Fenêtres modales
 	$("#link_config_panel").leanModal({closeButton: ".modal_close"});
 	$("#link_howto_update").leanModal({closeButton: ".modal_close"});
 	$("#link_about_us").leanModal({closeButton: ".modal_close"});
